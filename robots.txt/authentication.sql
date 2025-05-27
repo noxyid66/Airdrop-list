c22868a4ad603793628b06241e12e1c12e27549b
noxyid66 <167220482+noxyid66@users.noreply.github.com>
2025-05-27 19:27:38 +0000

    Add files via upload

---
+++ robots.txt/authentication.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: sql102.byetcluster.com
+-- Waktu pembuatan: 27 Bulan Mei 2025 pada 15.26
+-- Versi server: 10.6.19-MariaDB
+-- Versi PHP: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `if0_38739425_Airdrop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `authentication`
+--
+
+CREATE TABLE `authentication` (
+  `id` int(250) NOT NULL,
+  `auth` varchar(300) NOT NULL,
+  `pin` varchar(16) NOT NULL,
+  `user` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Dumping data untuk tabel `authentication`
+--
+
+INSERT INTO `authentication` (`id`, `auth`, `pin`, `user`) VALUES
+(1, '0x0RC0XW732566a403y2g692926P9Fb8K14EQ1612u51f566875779840', '251216', 'Noxyid66');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `authentication`
+--
+ALTER TABLE `authentication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `authentication`
+--
+ALTER TABLE `authentication`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
